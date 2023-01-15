a11c26b3ea540d17b12e021d6811fc37a28e8e37
Jisu Kim <hellojs242@gmail.com>
2023-01-15 12:20:51 +0900

    :sparkles: feat : tree insert and rotate 완료

--- includes/_rb_tree.hpp
+++ includes/_rb_tree.hpp
@@ -33,6 +33,13 @@ struct _rb_tree_node_base {
   base_ptr parent;
   base_ptr left;
   base_ptr right;
+
+  void _rb_tree_init(base_ptr p) {
+    color = RED;
+    parent = p;
+    left = NIL;
+    right = NIL;
+  }
 };
 
 template <typename Val>
@@ -42,10 +49,11 @@ struct _rb_tree_node : public _rb_tree_node_base {
 
   value_type value;
 
-  _rb_tree_node(const value_type& value = value_type(), link_type parent = 0,
-                link_type left = 0, link_type right = 0,
-                _rb_tree_color color = RED)
-      : value(value), parent(parent), left(left), right(right), color(color) {}
+  //_rb_tree_node(const value_type& value = value_type(), link_type parent = 0,
+  //              link_type left = 0, link_type right = 0,
+  //              _rb_tree_color color = RED)
+  //    : value(value), parent(parent), left(left), right(right), color(color)
+  //    {}
 };
 template <typename Compare>
 struct _rb_tree_key_compare {
@@ -721,26 +729,73 @@ void _insert_rebalance(bool left, _rb_tree_node_base* x, _rb_tree_node_base* p,
   _rb_tree_node_base* root = header.parent;
 
   // Initialize new node
-  x->parent = p;
-  x->left = NIL;
-  x->right = NIL;
-  x->color = RED;
+  x->_rb_tree_init(p);
 
   // insert
+  // root, leftmost, rightmost 를 갱신해준다.
+  // empty 이면 left 이다.
   if (left) {
-    p->left = x;
-    if (p == &header) {
+    p->left = x;         // also makes leftmost = x when p == header
+    if (p == &header) {  // root 자리에 들어갈 경우
       header.parent = x;
       header.right = x;
-    } else if (p == header.left) {
+      x->color = BLACK;
+    } else if (p == header.left) {  // leftmost update
       header.left = x;
     }
   } else {
     p->right = x;
-    if (p == header.right) {  // right-most
+    if (p == header.right) {  // rightmost update
       header.right = x;
     }
   }
+
+  // rotate
+  while (x != root && x->parent->color == RED) {  // header -> no pp
+    _rb_tree_node_base* const xpp = x->parent->parent;
+    if (x->parent == xpp->left) {                 // 부모가 왼쪽 자식임
+      _rb_tree_node_base* const xu = xpp->right;  // uncle
+      if (xu && xu->color == RED) {
+        // case 1
+        // 삼촌과 부모가 빨간색, 나는 왼쪽 자식 -> color 교체만 해주면 됨
+        // 이미 할아버지는 BLACK 인 게 확실함
+        x->parent->color = BLACK;
+        xu->color = BLACK;
+        xpp->color = RED;
+        x = xpp;  // 다시 확인할 노드를 할아버지 (xpp) 로 바꿔줌
+      } else {  // 부모가 왼쪽, 삼촌이 없거나 (이것도 BLACK) 삼촌이 검은색
+        if (x == x->parent->right) {  // 나 오른쪽 자식
+                                      // case 2
+          x = x->parent;
+          // 부모 기준으로 왼쪽 회전 후 case 3으로 넘어감
+          _rb_tree_rotate_left(x, root);
+        }
+        // case 3
+        // 부모와 할아버지의 색을 바꾸고 할아버지 기준으로 오른쪽 회전.
+        x->parent->color = BLACK;
+        xpp->color = RED;
+        _rb_tree_rotate_right(xpp, root);
+      }
+    } else {
+      // 순서만 바뀌고 위랑 똑같!!
+      _rb_tree_node_base* const xu = xpp->left;
+      if (xu && xu->color == RED) {
+        x->parent->color = BLACK;
+        xu->color = BLACK;
+        xpp->color = RED;
+        x = xpp;
+      } else {
+        if (x == x->parent->left) {
+          x = x->parent;
+          _rb_tree_rotate_right(x, root);
+        }
+        x->parent->color = BLACK;
+        xpp->color = RED;
+        _rb_tree_rotate_left(xpp, root);
+      }
+    }
+  }
+  root->color = BLACK;  // while 을 바로 빠져나온 경우를 위해 setting.
 }
 
 /**
